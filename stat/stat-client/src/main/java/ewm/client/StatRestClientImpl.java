d0d238c01b82ce190ec8fc7e5b8de66c6a1e18f3
Aleksandr Darsavelidze <darsavellis@yandex.ru>
2024-10-26 03:10:02 +0300

    Доработать Request API. Исправить ошибки тестов.

--- stat/stat-client/src/main/java/ewm/client/StatRestClientImpl.java
+++ stat/stat-client/src/main/java/ewm/client/StatRestClientImpl.java
@@ -28,10 +28,10 @@ public class StatRestClientImpl implements StatRestClient {
 
     public StatRestClientImpl(@Value("${stat-server.uri}") String baseUri) {
         this.restClient = RestClient.builder()
-                .baseUrl(baseUri)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .requestFactory(customRequestFactory())
-                .build();
+            .baseUrl(baseUri)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .requestFactory(customRequestFactory())
+            .build();
     }
 
     public void addHit(EndpointHitDto hitDto) {
@@ -45,16 +45,16 @@ public class StatRestClientImpl implements StatRestClient {
     public List<ViewStatsDto> stats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
         try {
             return restClient.get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path("/stats")
-                            .queryParam("start", start)
-                            .queryParam("end", end)
-                            .queryParam("uris", String.join(",", uris))
-                            .queryParam("unique", unique)
-                            .build())
-                    .retrieve()
-                    .body(new ParameterizedTypeReference<>() {
-                    });
+                .uri(uriBuilder -> uriBuilder
+                    .path("/stats")
+                    .queryParam("start", start)
+                    .queryParam("end", end)
+                    .queryParam("uris", String.join(",", uris))
+                    .queryParam("unique", unique)
+                    .build())
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
         } catch (Exception e) {
             log.info("Ошибка при запросе к эндпоинту /stats {}", e.getMessage());
             return Collections.emptyList();
@@ -63,8 +63,8 @@ public class StatRestClientImpl implements StatRestClient {
 
     private ClientHttpRequestFactory customRequestFactory() {
         ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
-                .withConnectTimeout(Duration.ofSeconds(10))
-                .withReadTimeout(Duration.ofSeconds(10));
+            .withConnectTimeout(Duration.ofSeconds(10))
+            .withReadTimeout(Duration.ofSeconds(10));
         return ClientHttpRequestFactories.get(settings);
     }
 }
--- stat/stat-server/src/main/java/ewm/repository/HitRepository.java
+++ stat/stat-server/src/main/java/ewm/repository/HitRepository.java
@@ -12,36 +12,36 @@ import java.util.List;
 public interface HitRepository extends JpaRepository<EndpointHit, Long> {
 
     @Query(value = "select new ewm.dto.ViewStatsDto(eh.app, eh.uri, count(eh.ip) as cnt) " +
-            "from EndpointHit eh " +
-            "where eh.timestamp >= :start and eh.timestamp <= :end " +
-            "group by eh.uri, eh.app " +
-            "order by cnt desc")
+        "from EndpointHit eh " +
+        "where eh.timestamp >= :start and eh.timestamp <= :end " +
+        "group by eh.uri, eh.app " +
+        "order by cnt desc")
     List<ViewStatsDto> getAllStats(@Param("start") LocalDateTime start,
                                    @Param("end") LocalDateTime end);
 
     @Query(value = "select new ewm.dto.ViewStatsDto(eh.app, eh.uri, count(distinct(eh.ip)) as cnt) " +
-            "from EndpointHit eh " +
-            "where eh.timestamp >= :start and eh.timestamp <= :end " +
-            "group by eh.uri, eh.app " +
-            "order by cnt desc")
+        "from EndpointHit eh " +
+        "where eh.timestamp >= :start and eh.timestamp <= :end " +
+        "group by eh.uri, eh.app " +
+        "order by cnt desc")
     List<ViewStatsDto> getAllStatsUniqueIp(@Param("start") LocalDateTime start,
                                            @Param("end") LocalDateTime end);
 
 
     @Query(value = "select new ewm.dto.ViewStatsDto(eh.app, eh.uri, count(eh.ip) as cnt) " +
-            "from EndpointHit eh " +
-            "where eh.timestamp >= :start and eh.timestamp <= :end and eh.uri in :uris " +
-            "group by eh.uri, eh.app " +
-            "order by cnt desc")
+        "from EndpointHit eh " +
+        "where eh.timestamp >= :start and eh.timestamp <= :end and eh.uri in :uris " +
+        "group by eh.uri, eh.app " +
+        "order by cnt desc")
     List<ViewStatsDto> getStats(@Param("uris") List<String> uris,
                                 @Param("start") LocalDateTime start,
                                 @Param("end") LocalDateTime end);
 
     @Query(value = "select new ewm.dto.ViewStatsDto(eh.app, eh.uri, count(distinct(eh.ip)) as cnt) " +
-            "from EndpointHit eh " +
-            "where eh.timestamp >= :start and eh.timestamp <= :end and eh.uri in :uris " +
-            "group by eh.uri, eh.app " +
-            "order by cnt desc")
+        "from EndpointHit eh " +
+        "where eh.timestamp >= :start and eh.timestamp <= :end and eh.uri in :uris " +
+        "group by eh.uri, eh.app " +
+        "order by cnt desc")
     List<ViewStatsDto> getStatsUniqueIp(@Param("uris") List<String> uris,
                                         @Param("start") LocalDateTime start,
                                         @Param("end") LocalDateTime end);
